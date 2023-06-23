49dea1c965f949b8521569c423a613cc0b8ad8e2
Mas-nobb <masimo1009@gmail.com>
2023-06-23 13:53:59 +0900

    コメントモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :prototypes
+  has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
