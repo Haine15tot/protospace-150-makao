bdf992c380f1668199f4203507e31e8358d4e6bf
shogo-ohya <136320612+shogo-ohya@users.noreply.github.com>
2023-06-23 12:54:17 +0900

    編集機能修正版

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,42 +1,60 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
   def index
     @prototypes = Prototype.all
   end
-
+  def show
+    # @comments = @prototype.comments
+  end
   def new
     @prototypes = Prototype.new
   end
-
   def create
-    @prototype = Prototype.new(prototype_params)
-    if @prototype.save
+    @prototypes = Prototype.new(prototype_params)
+    if @prototypes.save
       redirect_to root_path, notice: '投稿されました'
     else
       render :new, status: :unprocessable_entity
     end
   end
-
-  def show
-    @prototype = Prototype.find(params[:id])
-  end
-
   def edit
-     @prototype = Prototype.find(params[:id])
+    unless @prototypes.user == current_user
+      redirect_to root_path
+    end
   end
-  
-
   def update
-    @prototype = Prototype.find(params[:id])
-    if @prototype.update(prototype_params)
-      redirect_to root_path, notice: '更新されました'
+    if @prototypes.update(prototype_params)
+      redirect_to root_path(@prototypes), notice: '投稿が更新されました'
     else
       render :edit, status: :unprocessable_entity
     end
   end
-
+  def destroy
+    if @prototypes.user == current_user
+      @prototypes.destroy
+      redirect_to root_path, notice: '投稿が削除されました'
+    else
+      redirect_to root_path, notice: '投稿の削除に失敗しました'
+    end
+  end
   private
-
+  def set_prototype
+    @prototypes = Prototype.find(params[:id])
+  end
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
